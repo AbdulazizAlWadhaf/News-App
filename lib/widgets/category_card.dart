cf4697de8fcea76f0900654c872648a9b2e8f1ad
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-06-10 06:17:27 -0700

    category card

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({super.key, required this.category});
@@ -7,28 +8,35 @@ class CategoryCard extends StatelessWidget {
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10.0),
-      child: Container(
-              height: 100,
-              width: 200,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage(category.image)
-                  ),
-              ),
-              child: Center(
-                  child: Text(
-                      category.categoryName,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                      ),
-                  ),
-              ),
-          ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return CategoryView(category: category.categoryName);
+        }));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 10.0),
+        child: Container(
+                height: 100,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage(category.image)
+                    ),
+                ),
+                child: Center(
+                    child: Text(
+                        category.categoryName,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                        ),
+                    ),
+                ),
+            ),
+      ),
     );
   }
 }
