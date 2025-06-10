904265fa6c13809afd3c8a228e49c8eb609152fd
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-06-10 06:15:17 -0700

    create category view

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/widgets/NewsListViewBuilder.dart';
 
+class CategoryView extends StatelessWidget {
+  const CategoryView({super.key, required this.category});
+
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          NewsListViewBuilder(
+            category: category,
+          )
+        ],
+      ),
+    );
+  }
+}
